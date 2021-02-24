818fec288c1784b5a777662c1036e6f23a088186
ddmeyer23 <78511312+ddmeyer23@users.noreply.github.com>
2021-02-24 00:59:29 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+require(["esri/config","esri/Map", "esri/views/MapView","esri/widgets/BasemapToggle", "esri/widgets/BasemapGallery", "esri/layers/FeatureLayer", "esri/WebMap","esri/core/urlUtils"], 
+      
+        function (esriConfig,Map, MapView,BasemapToggle, BasemapGallery, FeatureLayer, WebMap, urlUtils){
+  
+  //csv files require CORS enabled server if not on same domain as your website - so proxy required//
+  var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv"
+  esriConfig.request.corsEnabledServers.push('https://rawgit.com');
+  
+  
+  const template = {
+    title: "Crime Index",
+    content: "Crime" 
+  };
+  
+  const csvLayer = new CSVLayer({
+    url: url,
+    copyright: "Crime in STL",
+    popupTemplate: template
+  });
+  
+  var symbol = {
+    type: "simple-marker",
+    color: "green",
+    size: 2
+  };
+  
+  csvLayer.renderer = {
+    type: "simple", //SimpleRenderer()
+    symbol: symbol
+  };
+  
+  const map = new Map({
+          basemap: "arcgis-nova", // Basemap layer service
+          layers: [csvLayer]
+        });
+  
+  
+  var view = new MapView({
+    map: webmap,
+    container: "viewDiv",
+    zoom: 6,
+    center: [-90.1994,38.6270]
+    
+  });
+});
